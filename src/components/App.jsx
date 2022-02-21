f9a81fd33fefe1374db9e7890af3b0f7c085583f
shvmsh20 <itsshivamsharma21@gmail.com>
2022-02-22 00:34:04 +0530

    adding notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
-
+import notes from "../notes";
 const App = ()=>{
     return <div>
         <Header/>
-        <Note/>
+        <Note title="t" content="c"/>
+        {notes.map( (note)=>{
+            return <Note key={note.key} title={note.title} content={note.content}/>
+        })}
         <Footer/>
     </div>
 }
